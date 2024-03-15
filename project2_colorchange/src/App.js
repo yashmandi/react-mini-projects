36b53fbe78b398867c865bb7578e48cc62740e28
yashmandi <yashmandi18@gmail.com>
2024-03-15 19:02:48 +0530

    Random Color Generator

---
+++ project2_colorchange/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import RandomColor from './components/RandomColor';
+
+function App() {
+  return (
+    <>
+      <RandomColor />
+    </>
+  );
+}
+
+export default App;
---
+++ project2_colorchange/src/components/RandomColor.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function RandomColor() {
+  return (
+    <div className='container'>
+      <button>Generate Random Color</button>
+    </div>
+  )
+}
+
+export default RandomColor
