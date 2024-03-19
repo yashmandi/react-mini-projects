2cb81ae87f78e4f74ec202f8fcfb2dbb2a43484f
yashmandi <yashmandi18@gmail.com>
2024-03-19 19:48:47 +0530

    qr code generator (6)

---
+++ project6-qrcode/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import QRCodeGenerator from './component/qrcode';
+
+function App() {
+  return (
+    <div>
+      <QRCodeGenerator />
+    </div>
+  );
+}
+
+export default App;
