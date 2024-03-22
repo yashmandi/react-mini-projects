83e34049de310672baebdd884eb67ca3e64a1671
yashmandi <yashmandi18@gmail.com>
2024-03-22 16:16:19 +0530

    tabs

---
+++ project9_tabs/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TabTest from './components/tabs-test';
+
+function App() {
+  return (
+    <div className="App">
+      <TabTest />
+    </div>
+  );
+}
+
+export default App;
