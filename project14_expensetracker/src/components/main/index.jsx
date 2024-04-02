4ab591c42685775b5784185de7fb77c59e5b4eef
yashmandi <yashmandi18@gmail.com>
2024-04-02 22:18:23 +0530

    expense tracker

---
+++ project14_expensetracker/src/components/main/index.jsx
@@ -0,0 +1,9 @@
+import { Flex } from '@chakra-ui/react';
+
+export default function Main() {
+    return <Flex textAlign={'center'} flexDirection={'column'} pr={'5'} pl={'5'} >
+        <Flex alignItems={'center'} justifyContent={'space-between'}>
+
+        </Flex>
+    </ Flex >
+}
